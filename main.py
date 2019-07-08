e89a39d7aaa251b4fd5ad927730f3b18334d59fd
dbh32 <51296575+dbh32@users.noreply.github.com>
2019-07-08 20:12:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
+def read_and_format():
+    '''Задача №1'''
+    global cook_book
+    cook_book = {}
+    with open('recipes.txt', encoding='utf-8') as f:
+
+        def create_dish():
+            '''Создание списков под каждое блюдо'''
+            dish = []
+            count = int(f.readline().strip())
+            cycle = 0
+            while cycle != count:
+                cycle += 1
+                dish.append(create_ingredients())
+            f.readline().strip()
+            return dish
+
+        def create_ingredients():
+            '''Наполняем списки блюд ингридиентами'''
+            l1 = f.readline().strip()
+            l2 = l1.split(' | ', 3)
+            # вспомогательные переменные
+            ingredients = {'ingredient_name': l2[0],
+                           'quantity': l2[1],
+                           'measure': l2[2]}
+            return ingredients
+
+        for line in f:
+            line = line.strip()
+            cook_book[line] = create_dish()
+
+
 def get_shop_list_by_dishes(dishes, person_count):
-    '''Функция по Задаче №2'''
+    '''Задача №2'''
+    read_and_format()
+    global result
     result = {}
     for dish in dishes:
-        if dish in cook_book.keys():
+        if dish not in cook_book.keys():
+            print(f'"{dish}" - нет рецепта или некорректный ввод :(')
+        else:
             ingredients = cook_book[dish]
             for ingredient in ingredients:
                 ingredient['quantity'] = int(ingredient['quantity']) * person_count
@@ -14,40 +50,37 @@ def get_shop_list_by_dishes(dishes, person_count):
                     d2 = d1['quantity'] + ingredient['quantity']
                     d1.update({'quantity': d2})
                     # вспомогательные переменные
-    # print(result)
-    # Задача №2
-    # Запуск функции на 53 строке
 
 
-def create_dish():
-    '''Создание списков под каждое блюдо'''
-    dish = []
-    count = int(f.readline().strip())
-    cycle = 0
-    while cycle != count:
-        cycle += 1
-        dish.append(create_ingredients())
-    f.readline().strip()
-    return dish
+def main():
+    print()
+    print('1 - Задача №1')
+    print('2 - Задача №2')
+    print('q - Выход')
+    while True:
+        print()
+        user_input = input('Выбор задачи: ')
+        print()
+        if user_input == '1':
+            read_and_format()
+            print(cook_book)
+        elif user_input == '2':
+            print('Формат ввода: Блюдо1, Блюдо2, Блюдо3, ...')
+            dish_input = input('Что готовим? ').split(', ')
+            ppl_input = int(input('На сколько человек? '))
+            if ppl_input <= 0:
+                print('Так не бывает :)')
+            else:
+                get_shop_list_by_dishes(dish_input, ppl_input)
+                print(result)
+        elif user_input == 'q':
+            print('До встречи!')
+            break
+        else:
+            print('Некорректная команда!')
 
 
-def create_ingredients():
-    '''Наполняем списки блюд ингридиентами'''
-    l1 = f.readline().strip()
-    l2 = l1.split(' | ', 3)
-    # вспомогательные переменные
-    ingredients = {'ingredient_name': l2[0],
-                   'quantity': l2[1],
-                   'measure': l2[2]}
-    return ingredients
+if __name__ == "__main__":
+    main()
 
-
-with open('recipes.txt', encoding='utf-8') as f:
-    cook_book = {}
-    for line in f:
-        line = line.strip()
-        cook_book[line] = create_dish()
-    # print(cook_book)
-    # Задача №1
-
-get_shop_list_by_dishes(['Омлет', 'Фахитос'], 3)
+# print(globals())
